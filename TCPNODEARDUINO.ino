70d1c024f5e87768b18f5aba7878d2367b3c7d9b
vinicius~~ <viniciusferreirwk@gmail.com>
2017-04-21 17:44:06 -0300

    fix

--- TCPNODEARDUINO.ino
+++ TCPNODEARDUINO.ino
@@ -1,23 +1,3 @@
-/**
- * @example TCPClientMultiple.ino
- * @brief The TCPClientMultiple demo of library WeeESP8266. 
- * @author Wu Pengfei<pengfei.wu@itead.cc> 
- * @date 2015.02
- * 
- * @par Copyright:
- * Copyright (c) 2015 ITEAD Intelligent Systems Co., Ltd. \n\n
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version. \n\n
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 #include "ESP8266.h"
 
 #define SSID        "Venizao"
@@ -26,19 +6,17 @@
 #define HOST_PORT   (8090)
 
 ESP8266 wifi(Serial1);
+static uint8_t mux_id = 0;
 
 #include "DHT.h"
 #define DHTPIN 2
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
-
 int pinopir = 7; 
 int retorno = 0;
 
 void(* resetFunc) (void) = 0; 
-
-static uint8_t mux_id = 0;
 
 void setup(void)
 {
@@ -47,42 +25,39 @@ void setup(void)
     
     Serial1.begin(115200);
     Serial.begin(9600);
-   
-    wifi.setOprToStation();
+    
+    //wifi.setOprToStation();
     if (wifi.joinAP(SSID, PASSWORD)) {
         Serial.print("CONECTADO ! IP: ");       
         Serial.println(wifi.getLocalIP().c_str());
+        wifi.enableMUX();
     } else {
         Serial.println("Reset arduino - join ap failure");
         resetFunc();
     }
-    
-    wifi.disableMUX();
-    if(!wifi.enableMUX()){
-      Serial.println("Reset arduino - enable mux failed");
-      //resetFunc();
-    }
-    
-    
-    
+    delay(1000);
+    dht.begin();
 }
 
+String paramsArduino = "";
+String luminosidade  = "";
+String temperatura   = "";
+String movimentacao  = "";
+int estado           = 0;
+float t              = 0;
+uint8_t buffer[128]  = {0};
+         
 void loop(void)
 {
 
     if (wifi.createTCP(mux_id, HOST_NAME, HOST_PORT)) {
-         int estado = analogRead(A5);  //Lê o valor fornecido pelo LDR  
-      
-         String paramsArduino = "";
-         String luminosidade = "#luminosidade:"+String(estado)+"";
-      
-         dht.begin();
-         float t = dht.readTemperature();
-               
-         String temperatura = "#temperatura:"+String(t)+"";
-         
-         retorno = digitalRead(pinopir);   
-         String movimentacao = "#movimentacao:"+String(retorno);
+   
+         estado       =  analogRead(A5);  //Lê o valor fornecido pelo LDR  
+         luminosidade = "#luminosidade:"+String(estado)+"";
+         t            = dht.readTemperature();
+         temperatura  = "#temperatura:"+String(t)+"";
+         retorno      = digitalRead(pinopir);   
+         movimentacao = "#movimentacao:"+String(retorno);
       
          paramsArduino.concat(luminosidade);
          paramsArduino.concat(temperatura);
@@ -94,10 +69,9 @@ void loop(void)
          if (wifi.send(mux_id, (const uint8_t*)params, strlen(params))) {
            Serial.println("Enviando: "+paramsArduino);
          }
+         paramsArduino = "";
          
-         uint8_t buffer[128] = {0};
-         uint32_t len = wifi.recv(mux_id, buffer, sizeof(buffer), 100);
-         
+         uint32_t len = wifi.recv(mux_id, buffer, sizeof(buffer), 100);         
          if (len > 0) {
             String str = (char*)buffer; 
             if(str.equals("liga_luz")){
@@ -112,11 +86,11 @@ void loop(void)
       
          if (!wifi.releaseTCP(mux_id)) {
           Serial.println("Reset arduino - release tcp");
-          //resetFunc();
+          resetFunc();
          }
      } else {
         Serial.println("Reset arduino - tcp not create");
-        //resetFunc();
+        resetFunc();
      }
 }
 
