a8b9d7ced46d7da93774c34e8bc4ac626b884f88
vinicius~~ <viniciusferreirwk@gmail.com>
2017-05-21 17:15:53 -0300

    fix

--- TCPNODEARDUINO.ino
+++ TCPNODEARDUINO.ino
@@ -1,14 +1,12 @@
-#include <EmonLib.h>
-
 #include "ESP8266.h"
 
 //configurações do wifi
-//#define SSID        "COBRE"
-//#define PASSWORD    "robotica"
+//#define   SSID        "COBRE"
+//#define   PASSWORD    "robotica"
 #define SSID        "Venizao"
 #define PASSWORD    "venizao123"
 
-#define HOST_NAME   "192.168.0.27"
+#define HOST_NAME   "192.168.0.20"
 #define HOST_PORT   (8090)
 //configurações do wifi
 
@@ -17,15 +15,13 @@ static uint8_t mux_id = 0;
 
 //DECLARACAO DOS DHT
 #include "DHT.h"
-#define DHTPIN 2
+#define DHTPIN  2
 #define DHTPIN2 3
 #define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
 DHT dht2(DHTPIN2, DHTTYPE);
 //DECLARACAO DOS DHT
-
-EnergyMonitor emon1;  
 
 int pinopir  = 7; 
 int pinopir2 = 6; 
@@ -38,19 +34,22 @@ void(* resetFunc) (void) = 0;
 
 void setup(void)
 {
-
     //set pino pir
     pinMode(pinopir, INPUT);
     pinMode(pinopir2, INPUT);
+    
     //set pino rele
     pinMode(10, OUTPUT);
     pinMode(11, OUTPUT); 
     pinMode(12, OUTPUT); 
+ 
     //se baudrate do wifi
     Serial1.begin(115200);
     Serial.begin(9600);
+    
     //set wifi para modo estaçãp
     wifi.setOprToStation();
+    
     //conecta wifi
     if (wifi.joinAP(SSID, PASSWORD)) {
         Serial.println(wifi.getLocalIP().c_str());
@@ -62,36 +61,18 @@ void setup(void)
     }
     dht.begin();
     dht2.begin();
-
     delay(1000);
 }
 
 //variaveis do escopo
-String paramsArduino = "";
-
-String luminosidade  = "";
-String luminosidade2 = "";
-
-String temperatura   = "";
-String temperatura2  = "";
-
-String movimentacao  = "";
-String movimentacao2  = "";
+String paramsArduino;
+char*  params;
 
 uint8_t buffer[128];
 
-int estado            = 0;
-int estado2           = 0;
-
-float t               = 0;
-float t2              = 0;
-
-char* params;
 //variaveis do escopo
 uint32_t len = 0;
-String str = "";
-
-
+//String str = "";
 
 void loop()
 {
@@ -103,11 +84,11 @@ void loop()
          //recebe dados do servidor
          len = wifi.recv(mux_id, buffer, sizeof(buffer), 500);         
          if (len > 0) {
-            //executa reles se vier comando do servidor
-             str = (char*)buffer;
+             char* str  = (char*)buffer;
             
             //regra shutdown
-            if(str.indexOf("shutdown_relays") != -1){
+            if (strcmp (str,"shutdown_relays") == 0) {
+
                 Serial.println("DESLIGANDO TUDO");
                 digitalWrite(10, HIGH); 
                 delay(200);
@@ -116,8 +97,9 @@ void loop()
                 digitalWrite(12, HIGH);
                 delay(200); 
             }
+    
+            if (strcmp (str,"up_app_relays") == 0) {
 
-            if(str.indexOf("up_app_relays") != -1){
                 Serial.println("LIGANDO TUDO");
                 digitalWrite(10, LOW);
                 delay(200); 
@@ -126,69 +108,47 @@ void loop()
                 digitalWrite(12, LOW);
                 delay(200); 
             }
-            
-            if(str.indexOf("abre_rele_luz1") != -1){
+
+            if (strcmp (str,"abre_rele_luz1") == 0) {
                 Serial.println("LIGANDO A LUZ");
                 digitalWrite(10, LOW);
             }
-            
-            if(str.indexOf("fecha_rele_luz1") != -1){
+
+            if (strcmp (str,"fecha_rele_luz1") == 0) {
                 Serial.println("DESLIGANDO A LUZ");
                 digitalWrite(10, HIGH); 
             }
 
-            if(str.indexOf("abre_rele_luz2") != -1){
+            if (strcmp (str,"abre_rele_luz2") == 0) {
                 Serial.println("LIGANDO A LUZ 2");
                 digitalWrite(11, LOW);
             }
-            
-            if(str.indexOf("fecha_rele_luz2") != -1){
+
+            if (strcmp (str,"fecha_rele_luz2") == 0) {
                 Serial.println("DESLIGANDO A LUZ 2");
                 digitalWrite(11, HIGH); 
             }
 
-
-            if(str.indexOf("abre_rele_luz3") != -1){
+            if (strcmp (str,"abre_rele_luz3") == 0) {
                 Serial.println("LIGANDO A LUZ 3");
                 digitalWrite(12, LOW);
             }
-            if(str.indexOf("fecha_rele_luz3") != -1){
+
+            if (strcmp (str,"fecha_rele_luz3") == 0) {
                 Serial.println("DESLIGANDO A LUZ 3");
                 digitalWrite(12, HIGH); 
             }
            
          }else{
 
-             paramsArduino = "";
-             //pega dados dos sensores
-             estado       =  analogRead(A5);  //Lê o valor fornecido pelo LDR  
-             luminosidade = "{ \"luminosidade\" : \""+String(estado)+"\" , ";
-
-             estado2        =  analogRead(A6);  //Lê o valor fornecido pelo LDR  
-             luminosidade2 = " \"luminosidade2\" : \""+String(estado2)+"\" , ";
+              paramsArduino       = "{ \"luminosidade\"  :  \"" +String(analogRead(A5))+"\"        , "    +
+                                    "  \"luminosidade2\"  : \""+String(analogRead(A6))+"\"         , "    +
+                                    "  \"temperatura\"    : \""+String(dht.readTemperature())+"\"  , "    +
+                                    "  \"temperatura2\"   : \""+String(dht2.readTemperature())+"\" , "    +
+                                    "  \"movimentacao\"   : \""+String(digitalRead(pinopir))+"\"   , "    +
+                                    "  \"movimentacao2\"  : \""+String(digitalRead(pinopir2))+"\" }  ";
+      
              
-             t            = dht.readTemperature();
-             temperatura  = "\"temperatura\" :\""+String(t)+"\" , ";
-
-             t2            = dht2.readTemperature();
-             temperatura2   = "\"temperatura2\" :\""+String(t2)+"\" , ";
-             
-             retorno      = digitalRead(pinopir);   
-             movimentacao = "\"movimentacao\" : \""+String(retorno)+"\" , ";
-             
-             retorno2      = digitalRead(pinopir2);   
-             movimentacao2 = "\"movimentacao2\" : \""+String(retorno2)+"\" } ";
-             
-             //pega dados dos sensores
-            
-             //cria dados para envio do servidor
-             paramsArduino.concat(luminosidade);
-             paramsArduino.concat(luminosidade2);
-             paramsArduino.concat(temperatura);
-             paramsArduino.concat(temperatura2);
-             paramsArduino.concat(movimentacao);
-             paramsArduino.concat(movimentacao2);
-  
              params = new char[paramsArduino.length()+1];
              strncpy(params, paramsArduino.c_str(), paramsArduino.length()+1);
              //cria dados para envio do servidor
@@ -209,6 +169,7 @@ void loop()
      } else {
         resetFunc();
      }
+     delay(1000);
 }
 
 
