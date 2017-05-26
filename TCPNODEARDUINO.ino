69af47e2efefce2bc3ac4dfe77fd5124de4c56ef
vinicius~~ <viniciusferreirwk@gmail.com>
2017-05-25 23:41:21 -0300

    fix

--- TCPNODEARDUINO.ino
+++ TCPNODEARDUINO.ino
@@ -135,7 +135,7 @@ void loop()
             dtostrf(dht.readTemperature(), 4, 2, temp1);
             dtostrf(dht2.readTemperature(), 4, 2, temp2);
                                                   
-            Serial.println(dht.readTemperature());                                                                                    
+            //Serial.println(dht.readTemperature());                                                                                    
             snprintf(szBuf, sizeBuf, "{ \"luminosidade\" : \" %d \", \"luminosidade2\" : \" %d \", \"temperatura\" : \"%s\", \"temperatura2\" : \"%s\", \"movimentacao\" : \" %d \"  \"movimentacao2\" : \" %d \"}", 
             analogRead(A5),
             analogRead(A6),
